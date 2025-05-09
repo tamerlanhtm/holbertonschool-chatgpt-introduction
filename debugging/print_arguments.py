581faaf7c6b6f404ef02320166bfdd51c841cf24
root <tamerlan_htm@hotmail.com>
2025-05-09 04:03:47 +0000

    print_arg

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -4,6 +4,6 @@ import sys
 if len(sys.argv) == 1:
     print("No arguments provided. Usage: ./script.py arg1 arg2 ...")
 else:
-    for arg in sys.argv:
+    for arg in sys.argv[1:]:
         print(arg)
 
